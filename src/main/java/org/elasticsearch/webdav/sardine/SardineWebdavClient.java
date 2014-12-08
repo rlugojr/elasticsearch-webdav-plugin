a8068b8811f3ea33af06a602c0d662a6d66e99a2
Alexey Korchevskiy <mitallast@gmail.com>
2014-12-08 15:30:14 +0300

    better output stream handle

--- src/main/java/org/elasticsearch/webdav/sardine/SardineWebdavClient.java
+++ src/main/java/org/elasticsearch/webdav/sardine/SardineWebdavClient.java
@@ -9,12 +9,10 @@ import org.elasticsearch.common.collect.MapBuilder;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.webdav.WebdavClient;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class SardineWebdavClient implements WebdavClient {
 
@@ -61,17 +59,34 @@ public class SardineWebdavClient implements WebdavClient {
 
     @Override
     public OutputStream createOutput(final URL url) throws IOException {
-        return new OutputStream() {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final PipedOutputStream out = new PipedOutputStream() {
             @Override
-            public void write(int b) throws IOException {
-                outputStream.write(b);
-            }
-
             public void close() throws IOException {
-                sardine.put(url.toString(), outputStream.toByteArray());
+                super.close();
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    // ignore
+                }
             }
         };
+        final PipedInputStream in = new PipedInputStream();
+        out.connect(in);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sardine.put(url.toString(), in);
+                    countDownLatch.countDown();
+                } catch (IOException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return out;
     }
 }
